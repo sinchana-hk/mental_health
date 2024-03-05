268fb050776c6b67c77567d7d943f8b71c2dc96d
sinchanahk <sinchanahk02@gmail.com>
2024-03-05 10:26:31 +0530

    Initional Commit

---
+++ mental health management system/Untitled-1.c
@@ -0,0 +1,16 @@
+int main()
+{
+  int m,n;
+  printf("enter the no of rows :");
+  scanf("%d",&m);
+  printf("enter the no of coloumn:");
+  scanf("%d",&n);
+  int array[m][n];
+  for(i=0;i<m;i++)
+  {
+    for(j=0;j<n;j++)
+    {
+      printf("%d",array);
+    }
+  }
+}
